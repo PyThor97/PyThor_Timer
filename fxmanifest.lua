3aa108039216b97da7078b2d3b6d6eb2f14de681
PyThor97 <pythor97il@gmail.com>
2025-04-01 00:01:56 +0300

    fxmanifest commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game { 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+name 'PyThor_Timer'
+author 'PyThor'
+description 'RedM script to start timer in game using NUI'
+lua54 'yes'
+
+shared_scripts { 'config.lua' }
+
+client_script { 'client/client.lua' }
+
+server_scripts { 'server/server.lua' }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^5PyThor_Timer ^4version Check^3'
+vorp_github 'https://github.com/PyThor97/PyThor_Timer'
